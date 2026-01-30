ef213a34966fdf5344d0b23659c93bc8cf04b8e6
Max Ilyin <skazo4ny@gmail.com>
2026-01-30 15:35:47 +0200

    updated the Auth0 configuration

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://backend:5000', // Backend service name in Docker network
   auth0: {
-    url: 'dev-biseljjjloqgmufv.us.auth0.com', // the auth0 domain prefix (without .auth0.com)
+    url: 'dev-biseljjjloqgmufv.us', // the auth0 domain prefix (without .auth0.com)
     audience: 'dev', // the audience set for the auth0 app
     clientId: '7leSthHeQWR43WD169aYgjTK0CiyNvH7', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 

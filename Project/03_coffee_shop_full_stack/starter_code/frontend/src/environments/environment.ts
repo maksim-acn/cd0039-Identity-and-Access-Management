7402bd18b5684370d26503aa492de604f2b216a0
Max Ilyin <skazo4ny@gmail.com>
2026-01-30 11:07:18 +0200

    created project implementation plan, resolved first TODOs in backend code.

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'udacity-fsnd', // the auth0 domain prefix
+    audience: 'dev', // the audience set for the auth0 app
+    clientId: 'sFzS7yG4K5L8Q9W0E2R3T4Y5U6I7O8P9', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
